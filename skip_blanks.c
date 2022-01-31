e0185cdfa6f581c9199828c1ad01ad88148bfe59
Flambo <oliver.boorstein@gmail.com>
2022-01-30 22:15:59 -0900

    first half of skip_blanks

---
+++ skip_blanks.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAXLINE 1000 // max input line size
+
+int findline(char line[], int maxline);
+
+int main(void)
+{
+    int len; // current line length
+    char line[MAXLINE]; // current line
+
+    while (( len = findline(line, MAXLINE)) > 0)
+    {
+        if (line[0] != '\n')
+            printf("%d: %s", len, line);
+    }
+    return 0;
+}
+
+int findline(char s[], int lim)
+{
+    int c,i,bound_i;
+
+    for (i = 0; (c=getchar()) != EOF && c != '\n'; ++i)
+    {
+        if (i < lim-1) {
+            s[i] = c;
+        }
+        bound_i = i;
+    }
+    if (bound_i > lim)
+        bound_i = lim;
+    if (c == '\n')
+    {
+        ++bound_i;
+        s[bound_i] = c;
+        ++i;
+    }
+    s[bound_i + 1] = '\0';
+
+    for (int j = bound_i - 1; j >= 0; --j) {
+        if (s[j] == '\t' || s[j] == 'a') {
+            s[j+1] = '\0';
+            s[j] = '\n';
+            --i;
+        }
+        else
+            break;
+    }
+
+    return i;
+}
