af432e01ea419307792d5e467cb4b9cb497c3a6b
Flambo <oliver.boorstein@gmail.com>
2022-03-23 23:09:58 -0800

    create acey_ducey.c

---
+++ acey_ducey.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+
+}
+
+void initializeGame()
+{
+
+}
+
+void printIntro()
+{
+
+}
+
+int* pickCards()
+{
+    // pick two cards for the other card to be inbetween
+    // pick other card
+}
+
+void printDollarAmount()
+{
+
+}
+
+void printChosenCards()
+{
+
+}
+
+int* getCardName(int card_value)
+{
+
+}
+
+int getBetAmount()
+{
+
+}
+
+void handleBet(int bet_amount)
+{
+    // too poor
+    // chickened out
+    // valid bet
+    // not a number
+}
+
+void handleWin(int bet_amount)
+{
+
+}
+
+void handleLose(int bet_amount)
+{
+
+}
+
+void restartGame()
+{
+
+}
