f22d72b45323378101d47c110ca762642e8e43fc
Flambo <oliver.boorstein@gmail.com>
2022-03-24 22:39:04 -0800

    Starting game loop

--- acey_ducey.c
+++ acey_ducey.c
@@ -2,25 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
-{
+#define START_CASH 100
+#define LOW_CARD 2
+#define HIGH_CARD 14
 
-}
+int currentCash;
+
 
 void initializeGame()
 {
-
+    currentCash = START_CASH;
 }
 
 void printIntro()
 {
-
+    printf("Acey-Ducey is played in the following manner\nThe dealer (computer) deals two cards face up\nYou have an option to bet or not bet depending\non whether or not you feel the card will have\na value between the first two.\nIf you do not want to bet, input a 0\n");
 }
 
-int* pickCards()
+int pickRandomCard()
 {
-    // pick two cards for the other card to be inbetween
-    // pick other card
+    int card = ("%i", (rand() % 13) + 2);
+
+    return card;
+}
+
+void pickRandomCards(int* cards)
+{
+    cards[0] = pickRandomCard();
+    do {
+        cards[1] = pickRandomCard();
+    } while (cards[1] == cards[0]);
+
+    do {
+        cards[2] = pickRandomCard();
+    } while (cards[2] == cards[0] || cards[2] == cards[1]);
 }
 
 void printDollarAmount()
@@ -64,4 +79,33 @@ void handleLose(int bet_amount)
 void restartGame()
 {
 
+}
+
+int main(void)
+{
+    srand(time(0));
+    initializeGame();
+    printIntro();
+    while (currentCash > 0) {
+        printf("You now have %i dollars\n\n", currentCash);
+        printf("Here are your next two cards\n");
+
+        int cards[3];
+
+        pickRandomCards((int*) cards);
+
+
+
+        if (cards[0] > cards[1]) {
+            cards[0] = cards[0] + cards[1];
+            cards[1] = cards[0] - cards[1];
+            cards[0] = cards[0] - cards[1];
+        }
+
+        printf("card: %i\n", cards[0]);
+        printf("card: %i\n", cards[1]);
+        printf("card: %i\n", cards[2]);
+
+        break;
+    }
 }
