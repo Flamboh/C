4c7deb1d4fd42b923327619466ad92ea5bd7923e
Flamboh <97427849+Flamboh@users.noreply.github.com>
2022-04-08 14:04:36 -0800

    Update war.c

--- war.c
+++ war.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+char *suits_str[4] = {"S", "H", "D", "C"};
+char *faces_str[13] = {"2", "3", "4", "5", "6", "7", "8", "9",
+            "10", "J", "Q", "K", "A"};
+
+
+/*
 int pickRandomCardNumber()
 {
     int card = (rand() % 13) + 2;
@@ -32,7 +38,7 @@ char pickRandomCardSuit()
     }
     return suit;
 }
-
+*/
 
 int main(void)
 {
@@ -41,5 +47,35 @@ int main(void)
     
 
     srand(time(0));
-    printf("%i\n%c\n", pickRandomCardNumber(), pickRandomCardSuit());
+    int score_you = 0;
+    int score_computer = 0;
+
+    int deck[52];
+    for (int i = 0; i < 52; i++) {
+        deck[i] = i;
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        /* shuffle by swapping cards pseudo-randomly a 1000 times */
+        int from = rand() % 52;
+        int to = rand() % 52;
+        int card = deck[from];
+        deck[from] = deck[to];
+        deck[to] = card;
+    }
+
+// for rounds
+// my card is deck[round]
+// computer card is deck[52 - round]
+// compare cards higher numeric value wins
+// increment score of winner
+// ask to continue
+// repeat if yes end if no
+// when rounds over compare scores
+
+
+
+//        int card = deck[i];
+//        int suit = card / 13;
+//        int face = card % 13;
 }
