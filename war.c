087ee42166b078a2c3eca184c4cb282fc152e7b5
Flambo <oliver.boorstein@gmail.com>
2022-04-11 20:23:34 -0800

    war done

--- war.c
+++ war.c
@@ -7,54 +7,26 @@ char *faces_str[13] = {"2", "3", "4", "5", "6", "7", "8", "9",
             "10", "J", "Q", "K", "A"};
 
 
-/*
-int pickRandomCardNumber()
-{
-    int card = (rand() % 13) + 2;
-
-    return card;
-}
-
-char pickRandomCardSuit()
-{
-    char suit;
-    int rand_int;
-
-    rand_int = (rand() % 4) + 1;
-
-    switch(rand_int) {
-        case 1 :
-            suit = 'S';
-            break;
-        case 2 :
-            suit = 'C';
-            break;
-        case 3 :
-            suit = 'H';
-            break;
-        case 4 :
-            suit = 'D';
-            break;
-    }
-    return suit;
-}
-*/
-
 int main(void)
 {
     printf("This is the card game of war. Each card is given by suit-#\n");
     printf("as S-7 for Spade 7.\n");
-    
+    printf("Do you want directions ");
+    char answer;
+    if((answer=getchar()) == 'y'|| answer != 'Y') {
+        printf("\nThe computer gives you and it a 'card'. The higher card\n");
+        printf("(numerically) wins. The game ends when you choose not to\n");
+        printf("continue or when you have finished the pack.\n\n");
+
+    }
 
     srand(time(0));
-    int score_you = 0;
-    int score_computer = 0;
-
+    int you_score = 0;
+    int computer_score = 0;
     int deck[52];
     for (int i = 0; i < 52; i++) {
         deck[i] = i;
     }
-
     for (int i = 0; i < 1000; i++) {
         /* shuffle by swapping cards pseudo-randomly a 1000 times */
         int from = rand() % 52;
@@ -63,6 +35,29 @@ int main(void)
         deck[from] = deck[to];
         deck[to] = card;
     }
+
+    for (int i = 0; i < 26; i++) {
+        int you_card = deck[i];
+        int cp_card = deck[51 - i];
+        int you_card_pt = you_card % 13;
+        int cp_card_pt = cp_card % 13;
+        printf("You: %s-%s  Computer: %s-%s\n", suits_str[you_card / 13], faces_str[you_card_pt], suits_str[cp_card / 13], faces_str[cp_card_pt]);
+        if (you_card_pt > cp_card_pt) {
+            you_score += 1;
+            printf("You win. You have %d and the computer has %d\n\n", you_score, computer_score);
+        }
+        else if (you_card_pt < cp_card_pt) {
+            computer_score +=1;
+            printf("The computer wins!!! You have %d and the computer has %d\n\n", you_score, computer_score);
+
+        }
+        else {
+            printf("Tie. No score change.\n\n");
+        }
+
+    }
+    printf("\nYou have run out of cards. ");
+    printf("Final score: You:%d Computer:%d", you_score, computer_score);
 
 // for rounds
 // my card is deck[round]
