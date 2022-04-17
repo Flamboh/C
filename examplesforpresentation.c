cfeef61fb400ca5b9d8f221514032c4f33ea90fd
Flambo <oliver.boorstein@gmail.com>
2022-04-17 15:52:03 -0800

    comments

--- examplesforpresentation.c
+++ examplesforpresentation.c
@@ -8,6 +8,17 @@ int myFunction(char letter) {
 
 int main(void)
 {
+int number;
+char letter = 'L';
+number = myFunction(letter);
+if (number == 3) {
+    letter = 'D';
+    number = myFunction(letter);
+}
+else {
+    letter = 'A';
+    number = 3;
+}
     // code here
 }
 
@@ -22,3 +33,7 @@ int main(void)
 //     letter = 'A';
 //     number = 3;
 // }
+#include <stdlib.h> // Standard Library. Includes lots of different things
+#include <time.h> // Anything to do with dates or time
+#include <string.h> // For manipulating strings
+#include <ctype.h> // for reading the types of variables or changing them
--- mini_calendar.c
+++ mini_calendar.c
@@ -1,51 +1,66 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int days_in_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // list of how many days are in each month
+// list of how many days are in each month
+int days_in_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 bool is_leap_year(int year) {
-  return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)); // formula for finding if a given year is a leap year
+  // formula for finding if a given year is a leap year
+  return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
 }
 
 void add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add) {
   int days_left_in_month;
-
-  while (days_left_to_add > 0) { // runs while there are still days left to add
-    days_left_in_month = days_in_month[*mm] - *dd; // calculates how many days are left in the month we started in
+  // runs while there are still days left to add
+  while (days_left_to_add > 0) {
+    // calculates how many days are left in the month we started in
+    days_left_in_month = days_in_month[*mm] - *dd;
     if (*mm == 2 && is_leap_year(*yy)) {
-      days_left_in_month++; // if it is february and it is a leap year then one extra day is added
+      // if it is february and it is a leap year then one extra day is added
+      days_left_in_month++;
     }
-    if (days_left_to_add > days_left_in_month) { // if there are more days to add than there are in the month
-      days_left_to_add -= days_left_in_month + 1; // added all days in this month so remove them from days left to add
-      *dd = 1; // first day of next month
+    // if there are more days to add than there are in the month
+    if (days_left_to_add > days_left_in_month) {
+      // added all days in this month so remove them from days left to add
+      days_left_to_add -= days_left_in_month + 1;
+      // first day of next month
+      *dd = 1;
       if (*mm == 12) {
-        *mm = 1; // set to january
-        *yy += 1; // if it is december and you overflow it is now january of the next year
+        // set to january
+        *mm = 1;
+        // if it is december and you overflow it is now january of the next year
+        *yy += 1;
       }
       else {
-        *mm += 1; // if it is not december just set it to the next month
+        // if it is not december just set it to the next month
+        *mm += 1;
       }
     }
-    else { // when there are less days to add then there are in the month
-      *dd += days_left_to_add; // then the day is the same as days left to add
-      days_left_to_add = 0; // resets to 0 so the loop ends
+    // when there are less days to add then there are in the month
+    else {
+      // then the day is the same as days left to add
+      *dd += days_left_to_add;
+      // resets to 0 so the loop ends
+      days_left_to_add = 0;
     }
   }
 }
 
 int main() {
-
+  // variables to store input
   int mm;
   int dd;
   int yy;
   int days_left_to_add;
-  // variables to store input
-  printf("Please enter a date between the years 1800 and 10000 in the format mm dd yy and provide the number of days to add to this date:"); // asking for input
-  scanf("%i%i%i%i", &mm, &dd, &yy, &days_left_to_add); // getting console input from the user and assigning it to the previously created variables
 
-  add_days_to_date(&mm, &dd, &yy, days_left_to_add); // calls the function with inputs from the user
+  // asking for input
+  printf("Please enter a date between the years 1800 and 10000 in the format mm dd yy and provide the number of days to add to this date:");
+  // getting console input from the user and assigning it to the previously created variables
+  scanf("%i%i%i%i", &mm, &dd, &yy, &days_left_to_add);
+  // calls the function with inputs from the user
+  add_days_to_date(&mm, &dd, &yy, days_left_to_add);
 
-  printf("%i %i %i\n", mm, dd, yy); // printing the results to the console
-
+  // printing the results to the console
+  printf("%i %i %i\n", mm, dd, yy);
 }
 
